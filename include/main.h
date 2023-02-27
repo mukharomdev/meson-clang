ffda8eef349152b9bde03b011587f630a0d475a2
mukharomdev <mukharomdev@gmail.com>
2023-02-27 15:04:13 +0700

    edit meson.build

--- include/main.h
+++ include/main.h
@@ -6,22 +6,23 @@
 #else
 #define __MESON_CLANG_THREADS 0
 #endif
+
+//###
 #if defined(__STDC_NO_ATOMICS__)
 #define __MESON_CLANG_ATOMICS 1
 #include <stdatomic.h>
 #else
-
 #define __MESON_CLANG_ATOMICS 0
 #endif
+//#####
 #if defined(__GNUC__) || defined(__linux__)
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+extern int thread_process(void);
+
 #endif
-
-int thread_process(void);
-
-
 #endif 
