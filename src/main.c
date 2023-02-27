d72410dcde821e36e56aead3ca0f486ecedf2f55
mukharomdev <mukharomdev@gmail.com>
2023-02-27 12:21:57 +0700

    first commit

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+
+int main() {
+    // printf("%d\n",__MESON_CLANG_THREADS );
+    // printf("%d\n",__MESON_CLANG_ATOMICS );
+    // printf("%d\n",__GNUC__);
+    thread_process();
+    return 0;
+}
